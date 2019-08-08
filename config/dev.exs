11cdbacee20582139bbeff2c555dfafc68d75160
Michael Gabriel <michaelngabriel@gmail.com>
2019-08-07 21:32:32 -0700

    "Fixed html generated file issues"

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :rumbl, Rumbl.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "**",
-  password: "*********",
+  username: "mg",
+  password: "@1061321B",
   database: "rumbl_dev",
   hostname: "localhost",
   pool_size: 10
